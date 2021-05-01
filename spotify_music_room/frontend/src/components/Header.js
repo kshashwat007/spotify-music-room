5592879fd734bbacf145b57eef98145205311c2f
kshashwat007 <kshashwat007@gmail.com>
2021-05-01 19:49:12 +0400

    header styles

--- spotify_music_room/frontend/src/components/Header.js
+++ spotify_music_room/frontend/src/components/Header.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider
+} from '@material-ui/core';
 
 const Header = () => {
+  const { header } = useStyles();
+
+  const logo = (
+    <ThemeProvider theme={theme}>
+      <Typography variant="h6" component="h1">
+        Music-Room
+      </Typography>
+    </ThemeProvider>
+  );
+
   return (
     <div style={{ marginBottom: 80 }}>
-      <AppBar>
-        <Toolbar>Spotify-Music-Room</Toolbar>
+      <AppBar elevation={1} className={header}>
+        <Toolbar>{logo}</Toolbar>
       </AppBar>
     </div>
   );
 };
+
+const theme = createMuiTheme({
+  typography: {
+    fontSize: 25,
+    fontFamily: 'Work Sans, sans-serif',
+    textAlign: 'center',
+    fontWeight: 700,
+    color: '#FFFEFE'
+  }
+});
+
+const useStyles = makeStyles(() => ({
+  header: {
+    backgroundColor: '#1DB954',
+    alignItems: 'center'
+  }
+}));
 
 export default Header;
