57b328822e583b071fb400c00f52c45ab0ea5744
kshashwat007 <kshashwat007@gmail.com>
2021-06-07 13:19:54 +0400

    fixes and restructuring misc

--- spotify_music_room/frontend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request, *args, **kwargs):
-    # render checks into the templates folder
-    return render(request, 'frontend/index.html')
